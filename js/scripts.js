3c9873421c85617e938c5d9b4103daecd3533da8
daherrerac <74057388+daherrerac@users.noreply.github.com>
2021-06-09 20:03:37 -0500

    Update
    Cortes adicionales y ajuste primer paquete

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,39 @@ if(document.getElementById('exampleModal')){
     })
     myModal.show();
 }
+
+$(window).on('load resize orientationchange', function() {
+    $('.arrendatario-slider').each(function(){
+        var $carousel = $(this);
+        /* Initializes a slick carousel only on mobile screens */
+        // slick on mobile
+        if ($(window).width() >= 992) {
+            if ($carousel.hasClass('slick-initialized')) {
+                $carousel.slick('unslick');
+            }
+        }
+        else{
+            if (!$carousel.hasClass('slick-initialized')) {
+                $carousel.slick({
+                    dots: false,
+                    arrows: true,
+                    infinite: true,
+                    slidesToShow: 2,
+                    prevArrow: '<a href="#" class="next"><img src="img/next.png" alt=""></a>',
+                    nextArrow: '<a href="#" class="prev"><img src="img/prev.png" alt=""></a>',
+                    responsive: [  
+                        {
+                            breakpoint: 767,
+                            settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1                            
+                            }
+                        }
+                    ]                   
+                });
+            }
+        }
+    });
+});
+
+
